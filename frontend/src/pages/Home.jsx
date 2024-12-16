d4acc11d5ef72f5e9baee359a17534ee363e55aa
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-16 17:43:16 +0430

    dev NewsletterBox component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import NewsletterBox from '../components/NewsletterBox'
 import OurPolicy from '../components/OurPolicy'
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <LatestCollection/>
       <BestSeller/>
       <OurPolicy/>
+      <NewsletterBox/>
     </div>
   )
 }
