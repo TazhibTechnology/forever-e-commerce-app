ae329c8b019234a75299c51b92a3a8987c076fe3
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-16 16:50:12 +0430

    dev our policy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import OurPolicy from '../components/OurPolicy'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
       <BestSeller/>
+      <OurPolicy/>
     </div>
   )
 }
