8ca2ca4c89b1834eccc925aade5b7d28af044701
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-19 14:00:31 +0430

    dev cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const cart = () => {
-  return (
-    <div>cart</div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
 
-export default cart
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: items,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    setCartData(tempData)
+  }, [cartItems]);
+  return (
+    <div className="border-t pt-14 ">
+      <div className="text-2xl mb-3">
+        <Title text1={'YOUR'} text2={'CART'}/>
+      </div>
+      <div>
+      {
+          cartData.map((item, index)=>{
+            const productData = products.find((product)=>product._id === item._id)
+            return(
+              <div key={index} className="py-4 border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
+                 <div className="flex items-start gap-6">
+                  <im className="w-16 sm:w-20" src={productData.image[0]} alt="" />
+                  <div>
+                    <p>{}</p>
+                  </div>
+                  <div/>
+                
+                <div/>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+};
+
+export default cart;
